bdbacbe51747c175f9ee55c0db0ae7ef5b9c9bbe
keshav <keshavjha68@gmail.com>
2020-03-23 21:24:17 +0530

    Updates

---
+++ precache-manifest.0a33d133066cef2881b9f10c24977901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c13063fd40e77f5dabfb5e2c45f8b41",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "3cfde9655da97584e57b",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/react/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfde9655da97584e57b",
+    "url": "/react/static/js/main.221917d5.chunk.js"
+  },
+  {
+    "revision": "e361f9f3976e228ea696",
+    "url": "/react/static/js/runtime-main.2844bc1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
