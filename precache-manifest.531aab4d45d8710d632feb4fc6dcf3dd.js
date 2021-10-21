8284f053aa164ec61f25247a219cc0642959aed1
keshav <keshavjha68@gmail.com>
2021-10-21 17:10:36 +0530

    Updates

---
+++ precache-manifest.531aab4d45d8710d632feb4fc6dcf3dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35df1796d75f1a510f7e45f384de1e27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06de47c3e3ee927f52cf",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "./static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06de47c3e3ee927f52cf",
+    "url": "./static/js/main.8827d12f.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
